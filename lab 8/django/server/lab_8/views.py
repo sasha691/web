26bfb71613bb790345437a2def2275c095cd1259
sasha691 <komp9349@gmail.com>
2023-12-08 02:19:56 +0200

    add form

--- lab 8/django/server/lab_8/views.py
+++ lab 8/django/server/lab_8/views.py
@@ -4,6 +4,26 @@ import json
 from .models import Player, Profession, Race
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        race_id = request.POST.get('race')
+        profession_id = request.POST.get('profession')
+
+        race = Race.objects.get(pk=race_id)
+        profession = Profession.objects.get(pk=profession_id)
+
+        banned=request.POST.get('banned')
+        banned = banned == 'on'
+
+        new_player = Player(
+            name=request.POST.get('name'),
+            title=request.POST.get('title'),
+            rase=race,
+            profession=profession,
+            level=request.POST.get('level'),
+            birthday=request.POST.get('birthday'),
+            banned=banned
+        )
+        new_player.save()
     player = Player.objects.all().values('id', 'name', 'title', 'rase__race', 'profession__profession', 'birthday', 'banned', 'level')
     player_list = list(player)
     for player in player_list:
